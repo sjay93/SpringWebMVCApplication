d33d7806b8c4d6998ca0e7126080d918ca564f73
Jay Shah <s.jay93@gmail.com>
2018-08-08 11:53:29 -0500

    Updated

--- src/com/yash/springmvcdemo/controller/HelloController.java
+++ src/com/yash/springmvcdemo/controller/HelloController.java
@@ -10,8 +10,8 @@ import java.util.Map;
 
 public class HelloController implements Controller {
     @Override
-    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        String message = httpServletRequest.getParameter("message");
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+        String message = request.getParameter("message");
         Map<String, String> map = new HashMap<>();
         map.put("msg", message);
         return new ModelAndView("welcome", map);
